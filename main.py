0779ecefd97653c084b0b6b6af17c19945a6ef7f
jack-jm <101677953+jack-jm@users.noreply.github.com>
2023-06-01 22:34:12 +0000

    Started on num_check function and set up.

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-dfxgdfgfdgghgh
+# Functions go here
+
+def num_check(question, error, num_type):
+  valid = False
+  while not valid:
+    try:
+      response = num_type(input(question))
+      if response <= 0:
+        print(error)
+      else:
+        return response
+    
+    except ValueError:
+      print(error)
+
+# Main routine goes here
+get_int = num_check("How many do you need? ", "Please enter an amount more than zero.\n", int)

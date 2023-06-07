49bc2b090470fe50ea36b71f9354b768571d4e77
jack-jm <101677953+jack-jm@users.noreply.github.com>
2023-06-07 03:08:13 +0000

    Continued on yes/no checker

--- main.py
+++ main.py
@@ -16,7 +16,28 @@ def num_check(question, error, num_type):
     except ValueError:
       print(error)
 
+def yes_no(question):
+  to_check = ["yes", "no"]
+  
+  valid = False
+  while not valid:
+    response = input(question).lower()
+    
+    for var_item in to_check:
+      if response == var_item:
+       valid = True
+       return response
+      elif response == var_item[0]:
+       valid = True
+       return var_item
+
+    print("Please enter either yes or no.")
+
 # Main routine goes here
+
+want_help = yes_no("Do you want to read the instructions? ")
+print("You said {}".format(want_help))
+
 # Gets number of items
 get_int = num_check("How many do you need? ", "Please enter an integer/whole number that is more than zero.\n", int)
 

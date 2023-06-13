5787ce3fec32a50b76907121d5d204b34b293d97
jack-jm <101677953+jack-jm@users.noreply.github.com>
2023-06-13 01:48:40 +0000

    Tried to do variable costs part 2 but failed somewhere

--- main.py
+++ main.py
@@ -49,6 +49,52 @@ def not_blank(question, error):
 def currency(x):
   return "${:.2f}".format(x)
 
+def get_expenses(var_fixed):
+  # Set up dictionaries and lists
+  item_list = []
+  quantity_list = []
+  price_list = []
+
+  variable_dict = {"Item": item_list,
+                   "Quantity": quantity_list,
+                   "Price": price_list}
+
+  # Loop to get component, quantity and price
+  item_name = ""
+  while item_name.lower() != "xxx":
+    print()
+    
+    # Get name, quantity and item
+    item_name = not_blank("Item Name: ", "The component name can not be blank")
+    if item_name.lower() == "xxx":
+      break
+  
+    quantity = num_check("Quantity: ", "The amount must be a whole number more than zero.", int)
+    
+    price = num_check("Price per single item: $", "The price must be a number more than zero.", float)
+  
+    # Add item, quantity, and price to lists
+    item_list.append(item_name)
+    quantity_list.append(quantity)
+    price_list.append(price)
+  
+  
+  expense_frame = pandas.DataFrame(variable_dict)
+  expense_frame = expense_frame.set_index('Item')
+  
+  # Calculate cost of each component
+  expense_frame['Cost'] = expense_frame['Quantity'] * expense_frame['Price']
+  
+  # Find sub-total
+  sub_total = expense_frame['Cost'].sum()
+  
+  # Currency Formatting (uses currency function)
+  add_dollars = ['Price', 'Cost']
+  for item in add_dollars:
+    expense_frame[item] = expense_frame[item].apply(currency)
+
+  return [expense_frame, sub_total]
+
 # Main routine goes here
 
 # Lists and dictionaries
@@ -64,6 +110,10 @@ variable_dict = {
 
 # Get user data
 product_name = not_blank("Product Name: ", "The product name cannot be blank")
+
+variable_expenses = get_expenses("variable")
+variable_frame = variable_expenses[0]
+variable_sub = variable_expenses[1]
 
 # Loop to get component, quantity, and price
 item_name = ""
